c0098d15aa1dc31bafd7aca6553546d10b47f07e
Muhammad Firdaus <mbex71@gmail.com>
2023-02-20 12:30:29 +0700

    add ant design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
+import 'antd/dist/reset.css';
 import type { AppProps } from 'next/app'
+import {ConfigProvider }from 'antd'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider
+      theme={{
+        components:{
+          Menu:{
+            colorItemBgHover:'#166088',
+            colorItemTextHover:'#DBE9EE',
+            colorItemBgSelected:'#166088',
+            colorItemTextSelected:'#DBE9EE'
+          }
+        }
+      }}
+    >
+
+      <Component {...pageProps} />
+    </ConfigProvider>
+  )
 }
